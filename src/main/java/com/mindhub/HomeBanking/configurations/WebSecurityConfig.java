7ad3fad5ff83ebfa4ebfe70fae2d1e472018fb4b
Santiago <santiago.aragon.99@gmail.com>
2022-10-19 18:52:43 -0300

    cambios peticiones axios de login

--- src/main/java/com/mindhub/HomeBanking/configurations/WebSecurityConfig.java
+++ src/main/java/com/mindhub/HomeBanking/configurations/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
